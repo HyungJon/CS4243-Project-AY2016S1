cabfa6be7ab47ae6de4227d66d31fab3f9ef0300
Hubert <Lvl100p@gmail.com>
2016-11-11 22:23:46 +0800

    add an extra zero for images from videos that have >= 1000 frames

--- extractAllFrames.py
+++ extractAllFrames.py
@@ -1,17 +1,19 @@
 import cv2
 import cv2.cv as cv
 
-cap = cv2.VideoCapture("beachVolleyball1.mov")
+cap = cv2.VideoCapture("beachVolleyball5.mov")
 fcount = int(cap.get(cv.CV_CAP_PROP_FRAME_COUNT))
 
 for fr in range(fcount):
     _, frame = cap.read()
     if fr % 2 == 0:
         filenum = str(fr+1)
-        if fr < 99:
-            filenum = "0" + filenum
-            if fr < 9:
-                filenum = "0" + filenum
+        if fr < 999:
+			filenum = "0" + filenum
+			if fr < 99:
+				filenum = "0" + filenum
+				if fr < 9:
+					filenum = "0" + filenum
         filename = "frame"+filenum+".jpg"
         cv2.imwrite(filename, frame)
         cv2.waitKey(10)

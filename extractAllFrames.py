8383dd167bce138691a0d56a46e48c3c8986de17
Hyung Jon Kim <hyungjon.kim@gmail.com>
2016-11-11 19:31:42 +0800

    Updated extractAlFrames to automatically add leading zeros

--- extractAllFrames.py
+++ extractAllFrames.py
@@ -7,7 +7,12 @@ fcount = int(cap.get(cv.CV_CAP_PROP_FRAME_COUNT))
 for fr in range(fcount):
     _, frame = cap.read()
     if fr % 2 == 0:
-        filename = "frame"+str(fr+1)+".jpg"
+        filenum = str(fr+1)
+        if fr < 99:
+            filenum = "0" + filenum
+            if fr < 9:
+                filenum = "0" + filenum
+        filename = "frame"+filenum+".jpg"
         cv2.imwrite(filename, frame)
         cv2.waitKey(10)
 
